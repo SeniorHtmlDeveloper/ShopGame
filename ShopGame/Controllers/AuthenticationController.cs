2c30f676a56685a22fcf7203140b21322398c474
MAMAT <MAMAT>
2022-09-20 21:16:20 +0300

    Подготовка почвы для Ламберта

---
+++ ShopGame/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopGame.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
