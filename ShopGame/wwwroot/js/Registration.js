cf32d68e73847e45bad12e9696076d62e8da245d
123 <user@DESKTOP-VUSU881>
2022-09-22 20:33:36 +0300

    add html css js for registration and signIn

---
+++ ShopGame/wwwroot/js/Registration.js
@@ -0,0 +1,35 @@
+﻿const inputList = document.querySelectorAll(".form__field")
+const labelList = document.querySelectorAll(".form__lab")
+
+
+for (let i = 0; i < inputList.length; i++) {
+    inputList[i].addEventListener('click', function () {
+        labelList[i].classList.add('form__lab__active');
+        console.log('add');
+    })
+}
+
+
+document.addEventListener('click', function (e) {
+    const box = e.composedPath().includes(document.querySelector("#reg__inner__1"))
+    if (!box) {
+        labelList[0].classList.remove('form__lab__active');
+        console.log('remove');
+    }
+})
+
+document.addEventListener('click', function (e) {
+    const box = e.composedPath().includes(document.querySelector("#reg__inner__2"))
+    if (!box) {
+        labelList[1].classList.remove('form__lab__active');
+        console.log('remove');
+    }
+})
+
+document.addEventListener('click', function (e) {
+    const box = e.composedPath().includes(document.querySelector("#reg__inner__3"))
+    if (!box) {
+        labelList[2].classList.remove('form__lab__active');
+        console.log('remove');
+    }
+})
---
+++ ShopGame/wwwroot/js/SignIn.js
@@ -0,0 +1,40 @@
+﻿const inputList = document.querySelectorAll(".form__field");
+const labelList = document.querySelectorAll(".form__lab");
+const innerlList = document.querySelectorAll(".sign-up__inner");
+
+
+console.log(inputList);
+console.log("test");
+
+
+
+document.addEventListener('click', function (e) {
+    const box = e.composedPath().includes(document.querySelector("#sign-up__inner__1"));
+    if (!box) {
+        labelList[0].classList.remove("form__lab__active");
+        console.log("remove");
+    }
+})
+
+document.addEventListener('click', function (e) {
+    const box = e.composedPath().includes(document.querySelector("#sign-up__inner__2"));
+    if (!box) {
+        labelList[1].classList.remove("form__lab__active");
+        console.log("remove");
+    }
+})
+
+
+// function onClickInput() {
+//     labelList.classList.Add(".form__lab__active");
+// };
+
+
+
+for (let i = 0; i < inputList.length; i++) {
+    inputList[i].addEventListener('click', function () {
+        labelList[i].classList.add("form__lab__active");
+
+        console.log("add");
+    });
+};
