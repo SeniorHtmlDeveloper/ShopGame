22b5b2ab16b78a71f9c7f58b4e7435f79c2d3d3a
MAMAT <MAMAT>
2022-09-23 09:51:57 +0300

    ne rabotayet

---
+++ ShopGame/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopGame.Models
+{
+	public class LoginModel
+	{
+        [Required(ErrorMessage = "Не указан Логин")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ ShopGame/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopGame.Models
+{
+	public class RegisterModel
+	{
+        [Required(ErrorMessage = "Не указан UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
